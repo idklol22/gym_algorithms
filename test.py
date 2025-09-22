08ab6fbe63fbd380ae66ea93ec851d4162da691a
idklol22 <wadhwasanjam@Gmail.com>
2025-09-22 21:41:59 +0530

    update

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 import gymnasium as gym
-
+import pandas as pd 
+import numpy as np
+import random
+learning_rate = 0.1
+discount_factor = 0.99
+epsilon = 1
+decay = 0.00001
+rng = np.random.default_rng(42)
+rng 
+print(rng)
 env = gym.make("FrozenLake-v1", map_name="8x8",is_slippery=False,render_mode="human")
 state =   env.reset()[0]
 terminated = False
